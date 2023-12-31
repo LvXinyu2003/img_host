279b088c9a819853a20b188be0e96e10f10677a2
LvXinyu2003 <l1418269455@gmail.com>
2023-12-31 13:48:02 +0800

    ui改进

--- main.cpp
+++ main.cpp
@@ -2,10 +2,18 @@
 
 #include <QApplication>
 
+#include <QPalette>
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     home_page w;
+
+    QPalette palette(w.palette());
+    palette.setColor(QPalette::Window, QColor("white"));
+    w.setAutoFillBackground(true);
+    w.setPalette(palette);
+
     w.show();
     return a.exec();
 }
